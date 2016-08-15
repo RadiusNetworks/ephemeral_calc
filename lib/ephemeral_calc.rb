e500f81f729a35b45862bb86f5ad3edf9b7374ab
Scott Yoder <yodersb@gmail.com>
2016-08-15 11:42:59 -0400

    refactor registration to make it simpler

--- lib/ephemeral_calc.rb
+++ lib/ephemeral_calc.rb
@@ -4,6 +4,7 @@ require "ephemeral_calc/version"
 require "ephemeral_calc/curve25519"
 require "ephemeral_calc/encryptor"
 require "ephemeral_calc/key_pair"
+require "ephemeral_calc/registration"
 
 module EphemeralCalc
   # Your code goes here...
---
+++ lib/ephemeral_calc/registration.rb
@@ -0,0 +1,89 @@
+module EphemeralCalc
+  class Registration
+
+    DEFAULT_NAMESPACE = "e3dd811dd3bbe49e630a"
+    DEFAULT_ROTATION_EXP = 12 # 2^12 = ~68 minutes
+
+    attr_reader :name,
+                :rotation_exp,
+                :namespace,
+                :instance,
+                :beacon_private_key,
+                :beacon_keypair,
+                :beacon_time_zero
+
+    def initialize(name:,
+                   rotation_exp: nil,
+                   namespace: nil,
+                   instance: nil,
+                   beacon_private_key: nil)
+      @name = name
+      @rotation_exp = (rotation_exp || DEFAULT_ROTATION_EXP).to_i
+      @namespace = namespace || DEFAULT_NAMESPACE
+      @instance = instance || random_instance
+      @beacon_private_key = beacon_private_key || EphemeralCalc::KeyPair.generate_private_key
+      @beacon_keypair = EphemeralCalc::KeyPair.new(@beacon_private_key)
+    end
+
+    def api_client
+      @api_client ||= ProximityBeacon::Client.new
+    end
+
+    def eid_params
+      @eid_params ||= api_client.eidparams
+    end
+
+    def resolver_public_key
+      Base64.decode64(eid_params["serviceEcdhPublicKey"])
+    end
+
+    def identity_key
+      beacon_keypair.identity_key(resolver_public_key: resolver_public_key)
+    end
+
+    def encryptor
+      @encryptor ||= EphemeralCalc::Encryptor.new(identity_key, rotation_exp)
+    end
+
+    def initial_eid
+      [encryptor.get_identifier(0)].pack("H*")
+    end
+
+    def register
+      beacon = ProximityBeacon::Beacon.new(
+        ephemeral_id_registration: {
+          beaconEcdhPublicKey: Base64.strict_encode64(beacon_keypair.public_key),
+          serviceEcdhPublicKey: eid_params["serviceEcdhPublicKey"],
+          rotationPeriodExponent: rotation_exp,
+          initialClockValue: encryptor.beacon_time,
+          initialEid: Base64.strict_encode64(initial_eid)
+        },
+        advertised_id: ProximityBeacon::AdvertisedId.new(
+          eddystone_ids: [namespace, instance]
+        ),
+        status: "ACTIVE",
+        description: self.name,
+      )
+      api_client.beacons.register(beacon)
+    end
+
+    def as_yaml
+      {
+        identity_key: identity_key,
+        rotation_exp: rotation_exp,
+        initial_time: encryptor.initial_time,
+      }
+    end
+
+    def to_yaml
+      as_yaml.to_yaml
+    end
+
+  private
+
+    def random_instance
+      SecureRandom.random_bytes(6).unpack("H*")[0]
+    end
+
+  end
+end
